f4b1f6d2652866ee8b9ff8297dd51dd7cf4c3fcd
Nada Mouhsine <nadamouhsine2@gmail.com>
2023-04-11 21:06:32 +0000

    accounts page

--- mon_vocabulaire/lib/main.dart
+++ mon_vocabulaire/lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:mon_vocabulaire/pages/quiz_page.dart';
 import 'package:mon_vocabulaire/Widgets/palette.dart';
 import 'Widgets/button.dart';
+=======
+import 'package:mon_vocabulaire/View/home.dart';
+import 'package:mon_vocabulaire/View/themes.dart';
+
+import 'View/Quiz/quiz_text_images.dart';
+import 'View/account.dart';
+import 'View/lesson_path.dart';
+import 'View/sub_themes.dart';
+>>>>>>> Stashed changes
 
 void main() {
   runApp(const MyApp());
