484567019eb605d8e5587f1ca924fa16182efabd
NiemaAM <niema.ama@gmail.com>
2023-04-12 11:03:33 +0000

    Adding logo minister in splash screen

---
+++ mon_vocabulaire/lib/View/splash_screen.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mon_vocabulaire/View/home.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _opacity = 1;
+      });
+    });
+  }
+
+  void goHome() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const Home(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    goHome();
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        width: double.maxFinite,
+        height: double.maxFinite,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              duration: const Duration(seconds: 1),
+              opacity: _opacity,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 5, right: 5, top: 50),
+                child: Image.asset(
+                  'assets/logo_ministere.png',
+                ),
+              ),
+            ),
+            const Expanded(
+              child: SizedBox(),
+            ),
+            AnimatedOpacity(
+              duration: const Duration(seconds: 1),
+              opacity: _opacity,
+              child: Column(
+                children: [
+                  Container(
+                    width: width / 3,
+                    height: width / 3,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          // ignore: unnecessary_string_interpolations
+                          image: AssetImage("assets/logo.png"),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "Mon vocabulaire",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Expanded(
+              flex: 3,
+              child: SizedBox(),
+            ),
+            const CircularProgressIndicator(),
+            const SizedBox(
+              height: 100,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
