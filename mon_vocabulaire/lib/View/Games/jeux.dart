dec3f7b6ab8ba28b53341cd16db501b9a45fec7b
NiemaAM <niema.ama@gmail.com>
2023-04-21 01:27:27 +0000

    cleaning previous pull request

--- mon_vocabulaire/lib/View/Games/jeux.dart
+++ mon_vocabulaire/lib/View/Games/jeux.dart
@@ -3,10 +3,6 @@ import '../../Model/user.dart';
 import '../../Widgets/Palette.dart';
 import '../../Widgets/game_bloc.dart';
 
-import '../../Widgets/Palette.dart';
-import '../../Widgets/appbargame.dart';
-import '../../Widgets/buttongame.dart';
-
 class Games extends StatefulWidget {
   final User user;
   const Games({super.key, required this.user});
@@ -16,12 +12,44 @@ class Games extends StatefulWidget {
 }
 
 class _GamesState extends State<Games> {
-  int chances = 3;
-  void song() async {}
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Games"),
-    );
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: GridView.count(
+        primary: false,
+        shrinkWrap: true,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: const [
+          GameBloc(
+            color: Palette.lightBlue,
+            image: "assets/themes_images/bubble.png",
+            price: '20',
+            text: "Jeu 1",
+          ),
+          GameBloc(
+            color: Palette.orange,
+            image: "assets/themes_images/drag_and_drop.png",
+            price: '120',
+            text: "Jeu 2",
+          ),
+          GameBloc(
+            color: Palette.lightGreen,
+            image: "assets/themes_images/images.png",
+            price: '50',
+            text: "Jeu 3",
+          ),
+          GameBloc(
+            color: Palette.pink,
+            image: "assets/themes_images/cloud.png",
+            price: '80',
+            text: "Jeu 4",
+          ),
+        ],
+      ),
+    ));
   }
 }
