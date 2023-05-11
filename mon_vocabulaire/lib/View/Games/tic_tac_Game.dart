d43644d16da0efaa8e50d73b7afaaa8a9ab0dffe
Soukaina <laguedanisoukaina@gmail.com>
2023-05-11 22:03:05 +0100

    tic tac toe game

--- mon_vocabulaire/lib/View/Games/tic_tac_Game.dart
+++ mon_vocabulaire/lib/View/Games/tic_tac_Game.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:mon_vocabulaire/Widgets/button.dart';
-import 'package:flutter_glow/flutter_glow.dart';
 
 import '../../Widgets/Palette.dart';
 
@@ -22,6 +21,8 @@ class tic_tac_state extends State<tic_tac>{
 Game game = Game();
 bool gameover = false;
 int randomNumber =0;
+bool variswin=false;
+
 @override
   void initState() {
     // TODO: implement initState
@@ -80,7 +81,7 @@ int randomNumber =0;
                                 Padding(
                                   padding: const EdgeInsets.all(10),
                                   child: Text(
-                                      player=="X" ? "Très bonne mission !" : "Ça y est, essaie encore.",
+                                      player=="assets/images/127.png" ? "Très bonne mission !" : "Ça y est, essaie encore.",
                                     style: const TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 25,
@@ -92,7 +93,7 @@ int randomNumber =0;
                                 Padding(
                                   padding: const EdgeInsets.only(bottom: 10),
                                   child: Text(
-                                    player=="X" ? "Félicitations, tu as fait du bon travail!" : "Ressayer !!",
+                                    player=="assets/images/127.png" ? "Félicitations, tu as fait du bon travail!" : "Ressayer !!",
                                     style: const TextStyle(
                                       fontSize: 16,
                                     ),
@@ -100,7 +101,7 @@ int randomNumber =0;
                                   ),
                                 ),
                                 Image.asset(
-                                  player=="X" ? "assets/mascotte/win.gif" : "assets/mascotte/lose.gif",
+                                  player=="assets/images/127.png" ? "assets/mascotte/win.gif" : "assets/mascotte/lose.gif",
                                   scale: 4,
                                 ),
                               ]),
@@ -126,124 +127,169 @@ int randomNumber =0;
   Widget build(BuildContext context) {
     double boardwidth = MediaQuery.of(context).size.width;
     double boardheight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      
-      body: 
-      Column(children: [
-        
-        const SizedBox(height: 30,),
-        Container(
-          width: boardwidth*0.6,
-          height: 60,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 244, 239, 239),
-            border: Border.all(
-              width: 2,
-              color: Colors.lightBlue
-            ),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("SALMA     ",style: TextStyle(color: Colors.pinkAccent,fontSize: 18, fontWeight: FontWeight.bold),),
-              Container(child: const Image(image: NetworkImage('https://cdn-icons-png.flaticon.com/512/3371/3371822.png'),),
-              decoration: BoxDecoration(border: Border.all(color: Colors.lightBlue), borderRadius:BorderRadius.circular(50))
-              ),         
-              const Text("        X ",style: TextStyle(color: Colors.pinkAccent,fontSize: 18, fontWeight: FontWeight.bold),),
-            ],
-          ),
-        ),
-        const SizedBox(height: 50,),
-        Container(  
-        width: boardwidth,
-        height: boardheight*0.8,
-        child: GridView.count(crossAxisCount: Game.boardlenth ~/ 3,
-        padding: const EdgeInsets.all(20.0),
-        mainAxisSpacing: 8.0,
-        crossAxisSpacing: 8.0,
-         children: 
-          List.generate(Game.boardlenth, (index){
-            return
-             InkWell(
-              onTap: gameover ?null:(){
-                setState(() {
-                  if(game.board![index]==""){
-                  game.board![index] = "X";
-                   Random random = new Random();
-                  List<int> casenum=[];
-                  for(int j=0; j<9;j++){
-                    if(game.board![j] == ""){
-                     casenum.add(j);
-                    }
-                  }
-                  int randomIndex = random.nextInt(casenum.length);
-                  randomNumber = casenum[randomIndex];
-                  print(randomNumber);
-                  game.board![randomNumber]="O";
-                  }
-                  if(game.board![0] == game.board![1] && game.board![1] == game.board![2] && game.board![0]!=""){
-                     iswin(game.board![0]);
-                  }else if(game.board![0] == game.board![3] && game.board![3] == game.board![6] && game.board![0]!=""){
-                     iswin(game.board![0]);
-                  }else if(game.board![0] == game.board![4] && game.board![4] == game.board![8] && game.board![0]!=""){
-                     iswin(game.board![0]);
-                  }else if(game.board![3] == game.board![4] && game.board![4] == game.board![5] && game.board![3]!=""){
-                     iswin(game.board![3]);
-                  }else if(game.board![6] == game.board![7] && game.board![7] == game.board![8] && game.board![6]!=""){
-                     iswin(game.board![6]);
-                  }else if( game.board![1] == game.board![4] && game.board![4] == game.board![7] && game.board![1]!=""){
-                     iswin(game.board![1]);
-                  }else if(game.board![2] == game.board![5] && game.board![5] == game.board![8] && game.board![2]!=""){
-                     iswin(game.board![2]);
-                  }else if(game.board![2] == game.board![4] && game.board![4] == game.board![6] && game.board![2]!=""){
-                     iswin(game.board![2]);
-                  }else{
-                    int count=0;
-                    for(int i=0;i<9;i++){
-                      if(game.board![i]!=""){
-                          count++;
+    return 
+//     Container(
+//       constraints: BoxConstraints.expand(),
+//       decoration : BoxDecoration(
+//         image: DecorationImage(image: AssetImage("assets/back3.jpg"),
+//        // colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.9), BlendMode.modulate,),
+//         fit: BoxFit.cover,
+// )
+    // child:  ),
+      Scaffold(
+     backgroundColor: Colors.white,
+
+        body:
+        Column(children: [
+    
+         SizedBox(height: 15,),
+          Container(  
+          width: boardwidth,
+          height: boardheight*0.72,
+          child: GridView.count(crossAxisCount: Game.boardlenth ~/ 3,
+          padding: const EdgeInsets.all(20.0),
+          mainAxisSpacing: 0.0,
+          crossAxisSpacing: 0.0,
+           children: 
+            List.generate(Game.boardlenth, (index){
+              return
+               InkWell(
+                onTap: gameover ?null:(){
+                  setState(() {
+                    if(game.board![index]==""){
+                    game.board![index] = "assets/images/127.png";
+                     Random random = new Random();
+                    List<int> casenum=[];
+                    for(int j=0; j<9;j++){
+                      if(game.board![j] == ""){
+                       casenum.add(j);
                       }
                     }
-                    if(count == 9){
-                      voidgame();
+                    int randomIndex = random.nextInt(casenum.length);
+                    randomNumber = casenum[randomIndex];
+                    print(randomNumber);
+                    game.board![randomNumber]="assets/images/128.png";
+                    
                     }
-                  }
-
-                });
-              },
-               child: Container(
-                width: Game.blocsize,
-                height: Game.blocsize,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 12, 12, 12),
-                  borderRadius: BorderRadius.circular(16.0),
-                 // border: Border.all(color: Colors.black),
-                  boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black45,
-                    blurRadius: 3,
-                    spreadRadius: 0.4,
-                    offset: Offset(2.0, 1),
-                  )
-                  ],
-                ),
-                child: Center(
-                  child: GlowText(game.board![index], style:
-                   TextStyle(color: game.board![index] == "X" 
-                   ?Colors.lightBlue
-                   : Colors.pinkAccent,
-                   fontSize: 64.0,
-                   ),),
-                ),
-                ),
-             );
-          })
-        ,),
-      ), 
-      ],)
-     
-     );}
+                    if(game.board![0] == game.board![1] && game.board![1] == game.board![2] && game.board![0]!=""){
+                     // variswin=true;
+                      // Future.delayed(Duration(seconds: 2), ()=>
+                       iswin(game.board![0]);
+                     //  );
+                    }else if(game.board![0] == game.board![3] && game.board![3] == game.board![6] && game.board![0]!=""){
+                     // variswin=true;
+                       iswin(game.board![0]);
+                    }else if(game.board![0] == game.board![4] && game.board![4] == game.board![8] && game.board![0]!=""){
+                       //variswin=true;
+                       iswin(game.board![0]);
+                    }else if(game.board![3] == game.board![4] && game.board![4] == game.board![5] && game.board![3]!=""){
+                     //   variswin=true;
+                       iswin(game.board![3]);
+                    }else if(game.board![6] == game.board![7] && game.board![7] == game.board![8] && game.board![6]!=""){
+                      //  variswin=true;
+                       iswin(game.board![6]);
+                    }else if( game.board![1] == game.board![4] && game.board![4] == game.board![7] && game.board![1]!=""){
+                      // variswin=true;
+                       iswin(game.board![1]);
+                    }else if(game.board![2] == game.board![5] && game.board![5] == game.board![8] && game.board![2]!=""){
+                     //   variswin=true;
+                       iswin(game.board![2]);
+                    }else if(game.board![2] == game.board![4] && game.board![4] == game.board![6] && game.board![2]!=""){
+                     //  variswin=true;
+                       iswin(game.board![2]);
+                    }else{
+                      int count=0;
+                      for(int i=0;i<9;i++){
+                        if(game.board![i]!=""){
+                            count++;
+                        }
+                      }
+                      if(count == 9){
+                        voidgame();
+                      }
+                    }
+    
+                  });
+                },
+                 child: Container(
+                  width: Game.blocsize,
+                  height: Game.blocsize,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                   // borderRadius: BorderRadius.circular(6.0),
+                    border: Border.all(color: Colors.black),
+                    boxShadow: const [
+                    BoxShadow(
+                      color: Color.fromARGB(115, 137, 197, 239),
+                      blurRadius: 3,
+                      spreadRadius: 0.4,
+                      offset: Offset(2.0, 1),
+                    )
+                    ],
+                  ),
+                  child: Center(
+                    child:
+                     game.board![index].isEmpty ? Text("")
+                     : Image.asset(game.board![index],width: 100,height: 100,),
+                  ),
+                  ),
+               );
+            })
+          ,),
+        ), 
+    
+        Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                  Container(
+                    width: boardwidth*0.3,
+                    height: 180,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(115, 137, 197, 239),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(child: const Image(image: NetworkImage('https://cdn-icons-png.flaticon.com/512/3371/3371822.png'),width: 50,height: 50,),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.pink), borderRadius:BorderRadius.circular(50))
+                        ), 
+                        const Text("SALMA",style: TextStyle(color: Colors.pink,fontSize: 18, fontWeight: FontWeight.bold),),        
+                        Image(image: AssetImage('assets/images/127.png'),width: 50,height: 50,)            ],
+                    ),
+                  ),
+                  SizedBox(width: 50,),
+                  Container(
+                    width: boardwidth*0.3,
+                    height: 180,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(115, 137, 197, 239),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(child: const Image(image: NetworkImage('https://cdn-icons-png.flaticon.com/128/2686/2686204.png'),width: 50,height: 50,),
+                        decoration: BoxDecoration(border: Border.all(color: Colors.pink), borderRadius:BorderRadius.circular(50))
+                        ), 
+                        const Text("CPU",style: TextStyle(color: Colors.pink,fontSize: 18, fontWeight: FontWeight.bold),),        
+                        Image(image: AssetImage('assets/images/128.png'),width: 50,height: 50,)            ],
+                    ),
+                  ),
+            ],
+          ),
+        ],)
+       
+       
+    );}
 }
 
 class player{
