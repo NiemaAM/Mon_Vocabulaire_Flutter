7671a602c1fe633add8eca22f53a952448e1953b
maroua-el-khalifi <marouaelkhalifi11@gmail.com>
2023-05-16 10:52:50 +0100

    modifying grammar errors and puzzle
    modifying grammar in tic tac toe , removing pop-up (temporarly for avoiding a bug) and adding numbers in sub themes path

--- mon_vocabulaire/lib/View/Games/tic_tac_toe.dart
+++ mon_vocabulaire/lib/View/Games/tic_tac_toe.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:mon_vocabulaire/Services/sfx.dart';
+import '../../Services/audio_background.dart';
 import '../../Widgets/Palette.dart';
 
 class TicTacToe extends StatefulWidget {
@@ -29,6 +30,13 @@ class TicTacToe_state extends State<TicTacToe> {
   void initState() {
     super.initState();
     game.board = Game.initGameBoard();
+    AudioBK.pauseBK();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    AudioBK.playBK();
   }
 
   bool Win() {
@@ -169,8 +177,8 @@ class TicTacToe_state extends State<TicTacToe> {
                   headerAnimationLoop: false,
                   dialogType: DialogType.info,
                   animType: AnimType.rightSlide,
-                  title: 'Resultat',
-                  desc: 'Tu as gagner',
+                  title: 'Résultat',
+                  desc: 'Tu as gagné',
                   btnOkText: "Tour suivant",
                   btnOkOnPress: () {})
               .show();
@@ -196,7 +204,7 @@ class TicTacToe_state extends State<TicTacToe> {
                   headerAnimationLoop: false,
                   dialogType: DialogType.info,
                   animType: AnimType.rightSlide,
-                  title: 'Resultat',
+                  title: 'Résultat',
                   desc: 'Tu as perdu',
                   btnOkText: "Tour suivant",
                   btnOkOnPress: () {})
@@ -284,10 +292,10 @@ class TicTacToe_state extends State<TicTacToe> {
             padding: const EdgeInsets.only(bottom: 10),
             child: Text(
               win > loose
-                  ? "Bravo, Tu as gagner !"
+                  ? "Bravo, Tu as gagné !"
                   : win == loose
                       ? "Égalité !"
-                      : "Dommage, Tu as perdue.",
+                      : "Dommage, Tu as perdu.",
               style: const TextStyle(
                 fontSize: 16,
               ),
